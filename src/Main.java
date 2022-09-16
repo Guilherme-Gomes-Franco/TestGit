763e140ce22c01f208bdbb29ca442e4773eda667
Guilherme <gg.franco@campus.fct.unl.pt>
2022-09-16 12:51:50 +0100

    Inicial Version do Main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Guilherme Franco
+ * Demo para criar um Git project
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Git demo");
+    }
+}
